7669e54ec16b6a6c837632100e7447488ffa81b8
zein12 <jena.zein@gmail.com>
2017-03-21 00:54:52 +0700

    Create composer.js

---
+++ composer.js
@@ -0,0 +1,13 @@
+{
+    "name": "BOT-TES/line-bot",
+    "description": "test line-bot",
+    "authors": [
+        {
+            "name": "TES",
+            "email": "jena.zein@gmail.com"
+        }
+    ],
+    "require": {
+        "linecorp/line-bot-sdk": "^1.3"
+    }
+}
